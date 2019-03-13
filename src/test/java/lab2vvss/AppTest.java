6f7f60a59e090efd9e2a193149b3e2efb22937dc
Iulia Lazar <iulia.lazar1998@yahoo.com>
2019-03-13 12:47:24 +0200

    Initial commit

---
+++ src/test/java/lab2vvss/AppTest.java
@@ -0,0 +1,25 @@
+package lab2vvss;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void addStudentTestHappyCase()
+    {
+        Student student= new Student();
+    }
+}
